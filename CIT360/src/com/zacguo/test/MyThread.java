1d752a263a59a84a376a82f058c4d115cc00864a
Z <Z@Z-W8.local>
2017-05-02 16:39:16 -0600

    05022017

--- CIT360/src/com/zacguo/test/MyThread.java
+++ CIT360/src/com/zacguo/test/MyThread.java
@@ -1,28 +1,22 @@
 package com.zacguo.test;
 
 public class MyThread extends Thread {
-	public void run(){
+	@SuppressWarnings("static-access")
+	public void run() {
 		for (int i = 0; i < 3; i++) {
-			System.out.println("Thread id: "
-                               +Thread.currentThread().getName());
+			System.out.println("Thread id: " + Thread.currentThread().getName());
 			try {
-				Thread.currentThread().sleep(100);
-			}
-			catch (Exception e) {
+				Thread.currentThread().sleep(500);
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	public static void main(String[] args){
-//        	for (int threadCnt = 0; threadCnt < 3; threadCnt++) {
-//           		 MyThread aThread =  new MyThread();
-//           		 aThread.start();
-//       		 }
-		
+
+	public static void main(String[] args) {
 		for (int threadCnt = 0; threadCnt < 3; threadCnt++) {
-			Thread t = new Thread(new MyRunnable());
-			t.start();
+			MyThread aThread = new MyThread();
+			aThread.start();
 		}
-		
 	}
 }
