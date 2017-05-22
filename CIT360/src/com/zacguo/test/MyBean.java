b04a9e436436ccda2f3895365715c83474a56c3f
Z <Z@Z-W8>
2017-05-21 18:12:40 -0600

    bean

---
+++ CIT360/src/com/zacguo/test/MyBean.java
@@ -0,0 +1,88 @@
+package com.zacguo.test;
+
+public class MyBean {
+	private String firstName;
+	private String lastName;
+	private int iNumber;
+	private int age;
+	
+	public MyBean(){
+		
+	}
+	
+	public MyBean(String firstName, String lastName, int iNumber, int age){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.iNumber = iNumber;
+		this.age = age;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getiNumber() {
+		return iNumber;
+	}
+
+	public void setiNumber(int iNumber) {
+		this.iNumber = iNumber;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + iNumber;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MyBean other = (MyBean) obj;
+		if (age != other.age)
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (iNumber != other.iNumber)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+}
